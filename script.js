0e97bcffa68c6384173fb1854ba56168ccf1df48
zkomac <zkomac@gmail.com>
2014-12-08 11:47:12 +0100

    Update html and script

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var board = new ChessBoard('board', 'start');
+var board2 = new ChessBoard('board2', {
+  draggable: true,
+  dropOffBoard: 'trash',
+  sparePieces: true
+});
+$('#startBtn').on('click', board2.start);
+$('#clearBtn').on('click', board2.clear);
